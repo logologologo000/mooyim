13a7b907ee0cf5edf43354930606d7cd4ee5ce09
ALOGOM <60058140+logologologo000@users.noreply.github.com>
2022-01-18 15:44:51 +0700

    index

--- src/index.js
+++ src/index.js
@@ -16,21 +16,47 @@ import LoginEmp from './components/employee/Login'
 import './css/mobile.css';
 import './css/employee.css';
 
+import {
+  BrowserRouter,
+  Routes, // Just Use Routes instead of "Switch"
+  Route,
+  Link,
+} from "react-router-dom";
+import Index from './components/mobile/Index';
 
+
+export default function Apps() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+
+        <Route path="/mobile" element={<IndexMobile />} />
+        <Route path="/mobile/beef" element={<div><NavMobile /><SelectbeefMobile /></div>} />
+        <Route path="/mobile/vet" element={<div><NavMobile /><SelectvetMobile /></div>} />
+        <Route path="/mobile/drink" element={<div><NavMobile /><SelectdrinkMobile /></div>} />
+        <Route path="/mobile/fast" element={<div><NavMobile /><SelectfastMobile /></div>} />
+
+        
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
 
+    <Apps />
 
-        <LoginEmp />
 
-    <NavMobile />
-    
 
-    
+
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
