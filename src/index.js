546debe87b95c9ca34a739b4c8175bb8d676421f
ALOGOM <60058140+logologologo000@users.noreply.github.com>
2022-01-19 19:34:30 +0700

    index m

--- src/index.js
+++ src/index.js
@@ -78,11 +78,13 @@ export default function Apps() {
         <Route path="/admin/table/tabledetail" element={<div><NavAdmin /><TabledetailAdmin /></div>} />
 
 
-        <Route path="/mobile" element={<IndexMobile />} />
-        <Route path="/mobile/beef" element={<div><NavMobile /><SelectbeefMobile /></div>} />
+        <Route path="/mobile/:tid" element={<IndexMobile />} />
+        <Route path="/mobile/:tid/beef" element={<div><NavMobile /><SelectbeefMobile /></div>} />
         <Route path="/mobile/vet" element={<div><NavMobile /><SelectvetMobile /></div>} />
         <Route path="/mobile/drink" element={<div><NavMobile /><SelectdrinkMobile /></div>} />
         <Route path="/mobile/fast" element={<div><NavMobile /><SelectfastMobile /></div>} />
+        <Route path="/mobile/end" element={<div><EndMobile /></div>} />
+        <Route path="/mobile/:tid/history" element={<div><HisMobile /></div>} />
         
         
 
